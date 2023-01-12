7172217ad6db8012c5b6a98f4fb96d92bc532ada
bbre1666 <badenbrenner@isu.edu>
2023-01-12 08:57:27 -0700

    Fancy Beep

--- week1example/Program.vb
+++ week1example/Program.vb
@@ -7,6 +7,15 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'Console.WriteLine()
+        'Console.Write("")
+        'Console.Beep(200, 1000)
+        Console.Beep(392, 500)
+        Console.Beep(440, 500)
+        Console.Beep(349, 750)
+        Console.Beep(196, 750)
+        Console.Beep(262, 900)
+
+
     End Sub
 End Module
